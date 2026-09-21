a87d4be5f25e098824e80e9616121feebd262c39
Mithil <mithil.pacharapalli.s.145@kalvium.community>
2026-09-21 09:47:24 +0530

    Wire service through repository

--- Module 4/Repository Pattern/src/services/postService.js
+++ Module 4/Repository Pattern/src/services/postService.js
@@ -1,43 +1,33 @@
-const postStore = require('../data/postStore');
+const postRepository = require("../repositories/postRepository");
 
 function listPosts() {
-  return postStore.posts;
+  return postRepository.findAll();
 }
 
 function getPost(id) {
-  return postStore.posts.find((post) => post.id === Number(id)) || null;
+  return postRepository.findById(id);
 }
 
 function createPost(fields) {
   if (!fields || !fields.title) {
-    const error = new Error('Title is required');
+    const error = new Error("Title is required");
     error.statusCode = 422;
     throw error;
   }
 
-  const post = {
-    id: postStore.nextId(),
+  return postRepository.create({
     title: fields.title,
-    body: fields.body || '',
+    body: fields.body || "",
     authorId: fields.authorId,
-  };
-  postStore.posts.push(post);
-  return post;
+  });
 }
 
 function updatePost(id, patch) {
-  const post = getPost(id);
-  if (!post) return null;
-  if (patch.title !== undefined) post.title = patch.title;
-  if (patch.body !== undefined) post.body = patch.body;
-  return post;
+  return postRepository.update(id, patch);
 }
 
 function removePost(id) {
-  const index = postStore.posts.findIndex((post) => post.id === Number(id));
-  if (index === -1) return false;
-  postStore.posts.splice(index, 1);
-  return true;
+  return postRepository.remove(id);
 }
 
 module.exports = { listPosts, getPost, createPost, updatePost, removePost };
